aab07b33e45725fa13bb9a1fc5553a38da424b0e
phoenix <lucbories07@gmail.com>
2024-02-03 00:15:52 +0100

    MetadataVideo

--- src/pages/api/dataVideo.ts
+++ src/pages/api/dataVideo.ts
@@ -4,14 +4,15 @@ const prisma = new PrismaClient()
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
     try {
-        const id = parseInt(JSON.parse(req.body).id)
-        
-        const posts = await prisma.videos.findUniqueOrThrow({ where: { id: id } })
-        
+        console.log("req")
+        const dataId: any = req.query.value;
+        const posts = await prisma.videos.findUniqueOrThrow({ where: { id: parseInt(dataId) } })
+
         await prisma.$disconnect()
         res.json(posts)
     }
     catch (error) {
+        console.log(error)
         await prisma.$disconnect()
         res.json("")
     }
