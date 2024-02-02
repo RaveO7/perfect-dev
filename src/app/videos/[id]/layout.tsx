e2c8a70bf5b3988a728a62ccda8b1813479d777b
phoenix <lucbories07@gmail.com>
2024-02-02 23:38:01 +0100

    testFetchVideo

--- src/app/videos/[id]/layout.tsx
+++ src/app/videos/[id]/layout.tsx
@@ -1,38 +1,38 @@
 import { Metadata, ResolvingMetadata } from 'next'
 
-export async function generateMetadata({ params }: { params: { id: any, test: any } }, parent: ResolvingMetadata): Promise<Metadata> {
-    const { id } = params;
+// export async function generateMetadata({ params }: { params: { id: any, test: any } }, parent: ResolvingMetadata): Promise<Metadata> {
+//     const { id } = params;
 
-    const apiUrlEndpoint = `${process.env.Site_URL}/api/dataVideo`
+    // const apiUrlEndpoint = `${process.env.Site_URL}/api/dataVideo`
 
-    const postData: any = {
-        method: "POST",
-        header: { "Content-Type": "application/json" },
-        body: JSON.stringify({ id: id })
-    }
-    const product = await fetch(apiUrlEndpoint, postData)
-    const res = await product.json()
+    // const postData: any = {
+    //     method: "POST",
+    //     header: { "Content-Type": "application/json" },
+    //     body: JSON.stringify({ id: id })
+    // }
+    // const product = await fetch(apiUrlEndpoint, postData)
+    // const res = await product.json()
 
-    const description = res.description.substring(0, 151) + ' ...';
+    // const description = res.description.substring(0, 151) + ' ...';
 
-    return {
-        title: `${res.title}`,
-        description: `${description}`,
-        keywords: ['bite'],
-        alternates: {
-            canonical: `${process.env.Site_URL}/videos/${id}?name=${encodeURI(res.title)}`,
-        },
-        openGraph: {
-            title: `${res.title}`,
-            description: `${description}`,
-            url: `${process.env.Site_URL}/videos/${id}?name=${encodeURI(res.title)}`,
-            siteName: "Perefect Porn",
-            images: [{
-                url: `${res.imgUrl}`,
-            }],
-            type: "website",
-        }
-    };
+    // return {
+    //     title: `${res.title}`,
+    //     description: `${description}`,
+    //     keywords: ['bite'],
+    //     alternates: {
+    //         canonical: `${process.env.Site_URL}/videos/${id}?name=${encodeURI(res.title)}`,
+    //     },
+    //     openGraph: {
+    //         title: `${res.title}`,
+    //         description: `${description}`,
+    //         url: `${process.env.Site_URL}/videos/${id}?name=${encodeURI(res.title)}`,
+    //         siteName: "Perefect Porn",
+    //         images: [{
+    //             url: `${res.imgUrl}`,
+    //         }],
+    //         type: "website",
+    //     }
+    // };
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) { return (<>{children}</>) }
