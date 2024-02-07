76724ef3df6745eee92facecceb0901d2d145efd
phoenix <lucbories07@gmail.com>
2024-02-07 23:13:16 +0100

    deletePackageJson

--- next.config.js
+++ next.config.js
@@ -15,12 +15,7 @@ const nextConfig = {
             }
         ]
     },
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-      },
-      
+    eslint: { ignoreDuringBuilds: true, },
 }
 
 module.exports = nextConfig
