2e8bdf2ac7d963e8fb275ed7e9a690022b58b045
phoenix <lucbories07@gmail.com>
2024-02-13 14:34:12 +0100

    delblur

--- src/components/ModalPub.tsx
+++ src/components/ModalPub.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react'
 
 export default function ModalPub() {
     const [openSearchBar, setOpenSearchBar] = useState(true)
-    const [randPub, setRandPub] = useState("")
+    const [randPub, setRandPub] = useState('https://candy.ai?via=hamelio73')
 
     // Fonction pour réinitialiser openSearchBar à true après 60 secondes
     const resetSearchBar = () => {
@@ -33,7 +33,7 @@ export default function ModalPub() {
         // https://candy.ai?via=hamelio73
         <Link href={randPub} role='link' aria-label={'Add new tab'} target="_blank" rel="noopener" onClick={() => setOpenSearchBar(false)} data-modal-backdrop="static" aria-hidden="true" className={`${openSearchBar ? "flex" : "hidden"}
         fixed top-0 right-0 left-0 justify-center items-center md:inset-0 
-        w-full h-full bg-tranparent backdrop-blur-sm z-[98] overflow-hidden`} >
+        w-full h-full bg-tranparent z-[98] overflow-hidden`} >
         </Link>
     )
 }
