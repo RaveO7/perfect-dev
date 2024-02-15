a318029125c03fc21af80900e8dc6e524ed3bcb1
phoenix <lucbories07@gmail.com>
2024-02-15 19:47:28 +0100

    modalPub

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" >
       <body className={inter.className} suppressHydrationWarning={true}>
         <MoreEighteen />
-        {/* <ModalPub /> */}
+        <ModalPub />
         <SpeedInsights />
         <Analytics />
 
