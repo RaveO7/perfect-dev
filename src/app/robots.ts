79b5aebc99bebbe8e2bc08637e2365ab32c3c034
RaveO7 <139851068+RaveO7@users.noreply.github.com>
2025-12-01 16:16:54 +0100

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,15 +2,35 @@ import type { MetadataRoute } from 'next'
 import { normalizeUrl } from '@/components/Utils'
 
 export default function robots(): MetadataRoute.Robots {
+    const siteUrl = normalizeUrl(process.env.Site_URL || '')
 
     return {
         rules: [
             {
                 userAgent: '*',
-                allow: ['/', '/videos', '/categorie', '/pornstar', '/channel', '/search'],
-                disallow: '/private/',
-            }
+
+                // ❗ Zones autorisées
+                allow: [
+                    '/',
+                    '/videos',
+                    '/categorie',
+                    '/pornstar',
+                    '/channel'
+                ],
+
+                // ❗ Zones à NE PAS indexer
+                disallow: [
+                    '/search',        // pages de recherche internes = très mauvaise idée en SEO
+                    '/private/',      // si tu as une zone privée
+                    '/*?*',           // empêche indexation des paramètres UTM & queries
+                    '/*&*',
+                ],
+            },
         ],
-        sitemap: normalizeUrl(process.env.Site_URL || '', 'sitemap.xml'),
+
+        // Ton sitemap racine DOIT être un sitemapindex
+        sitemap: normalizeUrl(siteUrl, 'sitemap.xml'),
+
+        host: siteUrl,
     }
 }
